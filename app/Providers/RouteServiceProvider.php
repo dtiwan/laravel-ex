d949c4d45d1c4fc4897136732c9d01736c436b33
Deepam Tiwan <deepamtiwan@gmail.com>
2018-04-11 22:10:08 -0500

    Making 5.2 compatible

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -15,6 +15,8 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     protected $namespace = 'App\Http\Controllers';
+    protected $webnamespace = 'App\Http\Controllers\web';
+    protected $apinamespace = 'App\Http\Controllers\api';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -37,9 +39,27 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map(Router $router)
     {
-        $this->mapWebRoutes($router);
+        //$this->mapWebRoutes($router);
 
-        //
+        /*
+        |--------------------------------------------------------------------------
+        | Web Router 
+        |--------------------------------------------------------------------------
+        */
+
+        $router->group(['namespace' => $this->webnamespace], function ($router) {
+            require base_path('routes/web.php');
+        });
+
+        /*
+        |--------------------------------------------------------------------------
+        | Api Router 
+        |--------------------------------------------------------------------------
+        */
+
+        $router->group(['namespace' => $this->apinamespace], function ($router) {
+            require base_path('routes/api.php');
+        });
     }
 
     /**
@@ -50,12 +70,42 @@ class RouteServiceProvider extends ServiceProvider
      * @param  \Illuminate\Routing\Router  $router
      * @return void
      */
-    protected function mapWebRoutes(Router $router)
+    protected function mapRoutes(Router $router)
     {
         $router->group([
             'namespace' => $this->namespace, 'middleware' => 'web',
         ], function ($router) {
             require app_path('Http/routes.php');
         });
+    }
+
+
+    /**
+     * Define the "web" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+    protected function mapWebRoutes()
+    {
+        Route::middleware('web')
+             ->namespace($this->namespace)
+             ->group(base_path('routes/web.php'));
+    }
+
+    /**
+     * Define the "api" routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapApiRoutes()
+    {
+        Route::prefix('api')
+             ->middleware('api')
+             ->namespace($this->namespace)
+             ->group(base_path('routes/api.php'));
     }
 }
--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        //Illuminate\Foundation\Http\Middleware\VerifyPostSize::class,
 
         /*
          * Package Service Providers...
